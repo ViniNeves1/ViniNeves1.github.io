d8cd90cfa14d62d8605c6b4f3f9819da8f9d17c8
ViniNeves1 <vinisfn097@gmail.com>
2024-08-28 14:29:15 -0300

    updates

--- js/home.js
+++ js/home.js
@@ -1,15 +1,3 @@
-$('.novidades button').click(function() {
-  $('.novidades').addClass('painel-aberto');
+$('.painel button').click(function() {
+  $(this).parent().toggleClass('painel-aberto');
 });
-
-$('.novidades button').click(function() {
-  $('.novidades').toggleClass('painel-aberto');
-});
-
-$('.vendidos button').click(function() {
-  $('.vendidos').addClass('painel-aberto');
-});
-
-$('.vendidos button').click(function() {
-  $('.vendidos').toggleClass('painel-aberto');
-})
